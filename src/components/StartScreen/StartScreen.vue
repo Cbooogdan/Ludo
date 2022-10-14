19430768659cefc68d2671442ab61242f0d358ef
Bogdan Corolea <bcorolea@softia.ro>
2022-10-15 02:11:56 +0300

    Add start screen component

---
+++ src/components/StartScreen/StartScreen.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="start-screen">
+        <div class="start-screen__buttons">
+            <button
+                    class="button"
+                    @click="handleStart"
+            >
+                Play Local
+            </button>
+
+            <button
+                    class="button"
+                    disabled
+            >
+                Play Online
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+import './StartScreen.scss';
+import { mapActions } from 'vuex';
+
+export default {
+    name: 'StartScreen',
+
+    methods: {
+        ...mapActions({
+            increaseCurrentGameStep: 'increaseCurrentGameStep',
+        }),
+
+        handleStart() {
+            this.increaseCurrentGameStep();
+        },
+    }
+};
+</script>
